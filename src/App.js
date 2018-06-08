5b79e90c109e449ca020c0f5ffc60e0fde0afbdd
Mantas Kaveckas <mantas.kaveckas@gmail.com>
2018-06-08 17:29:45 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1>
+      Mantas Kaveckas
+      <small>Senior Frontend Engineer</small>
+    </h1>
+    <a href="mailto:mantas.kaveckas@gmail.com">mantas.kaveckas@gmail.com</a>{" "}
+    <span class="separator">|</span>{" "}
+    <a target="_blank" href="https://www.linkedin.com/mantaskaveckas">
+      Linkedin
+    </a>{" "}
+    <span class="separator">|</span>{" "}
+    <a target="_blank" href="https://twitter.com/kaveckas">
+      @kaveckas
+    </a>
+  </div>
+);
