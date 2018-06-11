c280031db934c08f846f83480d60d34e06dbbd08
Mantas Kaveckas <mantas.kaveckas@gmail.com>
2018-06-11 12:00:50 +0300

    add ga

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
+import ReactGA from "react-ga";
 import App from "./App";
 import "./style.css";
+
+ReactGA.initialize("UA-104473382-2");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 render(<App />, document.getElementById("root"));
