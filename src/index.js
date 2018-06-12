4ce14383cfa8603646fb96677a4d9491713ee9b2
Mantas Kaveckas <mantas.kaveckas@gmail.com>
2018-06-12 23:03:51 +0300

    add font awesome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
-import "./assets/style.css";
+
+import "./assets/css/style.css";
+import "./assets/css/fa-regular.css";
+import "./assets/css/fa-brands.css";
+import "./assets/css/fontawesome.css";
 
 render(<App />, document.getElementById("root"));
